ac69b757c57b96ac57280e894a8bd5c70b119a54
VfanLee <fanfanfafafa@gmail.com>
2026-03-07 18:31:49 +0800

    feat: add shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,12 @@ const eslintConfig = defineConfig([
     'out/**',
     'build/**',
     'next-env.d.ts',
+    // shadcn
+    'src/components/ui/**',
+    'src/lib/utils.ts',
+    'components.json',
+    // others
+    // ...
   ]),
 ])
 
