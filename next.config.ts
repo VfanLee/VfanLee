79c1abe9b738e08c094baa00f413df497b021540
VfanLee <fanfanfafafa@gmail.com>
2026-03-10 16:36:12 +0800

    chore: update next.config.ts to set output to standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next'
 
+// https://nextjs.org/docs/app/api-reference/config/next-config-js
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
 }
 
 export default nextConfig
