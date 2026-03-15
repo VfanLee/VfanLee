a91bddad1b3653310bfd99f27621c621bdf1d2b5
VfanLee <fanfanfafafa@gmail.com>
2026-03-15 14:44:54 +0800

    chore: setup github pages deployment via github actions

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from 'next'
 
 // https://nextjs.org/docs/app/api-reference/config/next-config-js
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
